9490af22e3389d95324d20e0185f263d2437bce6
s0901598 <s0901598@gmail.com>
2025-09-14 17:08:21 +0800

    loading mask

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  loadingmask:boolean = true;
   title = 'yeshunweb';
+   ngOnInit(): void {
+      setTimeout(()=> {
+        this.loadingmask = false;
+        document.body.style.overflow = 'auto'
+      },5000);
+  }
 }
